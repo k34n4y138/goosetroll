07a6208e9661803760b32e93fb328fad66fd945c
zakaria moumen <zakariamoumen138@gmail.com>
2023-01-24 17:25:32 +0100

    new batch of memes + setup celar

--- setup.sh
+++ setup.sh
@@ -1,12 +1,5 @@
 #!/bin/zsh
 
-if [ ! -d "~/Library/.geezehidden/" ]
-then
-  git clone https://github.com/k34n4y138/goosetroll ~/Library/.geezehidden
-  cd ~/Library/.geezehidden/ && zsh ~/Library/.geezehidden/setup.sh
-else #executed for the second time inside the repo, time to reset memes]
-  echo removing files
-  rm -rf ~/Library/Containers/net.namedfork.DesktopGoose
-  rm -rf ~/Library/.geezehidden/Contents/Resources/{Memes,Notes}
-  python3 ~/Library/.geezehidden/main.py
-fi
+git clone https://github.com/k34n4y138/goosetroll ~/Library/.geezehidden &> /dev/null
+cd ~/Library/.geezehidden
+git pull &> /dev/null && rm -rf ~/Library/Containers/net.namedfork.DesktopGoose && rm -rf ~/Library/.geezehidden/Contents/Resources/{Memes,Notes} && python3 ~/Library/.geezehidden/main.py
